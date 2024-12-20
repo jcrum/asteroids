c89bfd81a1b2d4eceac0686b75437f659b628608
jcrum <me@jasonmcrum.com>
2024-12-20 01:17:50 -0900

    UPDATE player movement

--- main.py
+++ main.py
@@ -4,18 +4,11 @@ from player import Player
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: 1280")
-    print(f"Screen height: 720")
-
-    pygame.init()
-    
-    clock = pygame.time.Clock()
-    dt = 0
+    pygame.init() 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-
+    clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+    dt = 0
 
     # Game Loop
     while True:
@@ -23,10 +16,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        player.update(dt)
+        
         screen.fill("black")
-
         player.draw(screen)
-
         pygame.display.flip()
 
         time_passed = clock.tick(60)
